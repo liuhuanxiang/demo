744fcf667819c5e219c01c7a35e78de53ad62004
liuhuanxiang <liuhx@yocyl.com>
2022-08-23 16:12:06 +0800

    2

---
+++ design-patterns/src/main/java/build/Student.java
@@ -0,0 +1,18 @@
+package build;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class Student {
+    int id;
+    int age;
+    int grade;
+    String name;
+    String college;
+    String profession;
+    List<String> awards;
+
+
+}
